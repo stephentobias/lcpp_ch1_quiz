7161974bc1e48b9d4c7c2a0ef861f102ee8b520c
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 16:32:08 -0600

    Rename primary file and update Cmake files

--- lcpp_ch1_quiz.h
+++
@@ -1,8 +0,0 @@
-﻿// lcpp_ch1_quiz.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
