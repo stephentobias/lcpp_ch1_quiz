7a1f7bbf403b14a249f8458d4f0175799f0f3e64
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 16:41:23 -0600

    First iteration of program

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	int first_user_input{ };
+	int second_user_input{ };
+
+	std::cout << "Enter an integer: ";
+	std::cin >> first_user_input;
+
+	std::cout << "Enter another integer: ";
+	std::cin >> second_user_input;
+
+	std::cout << first_user_input << " + " << second_user_input << " = " 
+		<< first_user_input + second_user_input << ".\n";
+	std::cout << first_user_input << " - " << second_user_input << " = " 
+		<< first_user_input - second_user_input << ".\n";
+	
 	return 0;
 }
